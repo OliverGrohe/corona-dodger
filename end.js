8f21520b5bca186e5b3c6cfd54a83701f4aca4b6
Oliver Grohe <o.grohe@gmail.com>
2021-01-22 16:07:42 +0100

    main page

---
+++ end.js
@@ -0,0 +1,12 @@
+{/* <section class="end">
+        <div>
+            <h1>LOCKDOWN</h1>
+            <p>You cant spread anymore!</p>
+        </div>
+        <div>
+            !--DISPLAY SCORE--
+        </div>
+        <div>
+            <button>RESTART</button>
+        </div>
+    </section> */}
---
+++ game.js
@@ -0,0 +1,10 @@
+{/* <section class="game">
+        <canvas width="700px" height="350px"></canvas>
+</section> */}
+
+
+
+
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
+canvas.style.border = '2px solid black'
