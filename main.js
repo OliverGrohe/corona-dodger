671acac5ba71803121563552c026d1c251e93f15
Oliver Grohe <o.grohe@gmail.com>
2021-01-25 15:43:53 +0100

    elemets moving

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 let body = document.querySelector("body")
-//console.log(body)
+let canvas = ""
+let ctx = ""
+let virusX = 30
+let virusY = 175
+let maskX = 950
+let maskY = 50
+let lockdownX = 950
+let lockdownY = 150
+let partyX = 950
+let partyY = 350
 
 // Changing section-tag
 function deleteSectionOne() {
@@ -17,8 +26,6 @@ function mainToGame(){
         <script src="main.js"></script>
         `
   body.appendChild(sectionTwo)
-  //console.log(body)
-  //console.log(sectionTwo)
 }
 
 // When Start-button is clicked
@@ -27,7 +34,15 @@ function clickStart() {
   startBtn.addEventListener("click", function () {
     deleteSectionOne()
     mainToGame()
+    canvas = document.querySelector('canvas')
+    ctx = canvas.getContext('2d')
+    canvas.style.border = '2px solid black'
+
+    setInterval(function(){
+      requestAnimationFrame(draw)
+    }, 500)
   })
+  
 }
 clickStart()
 
@@ -36,36 +51,156 @@ clickStart()
 //---------------------------------------------------------------
 //---------------------------------------------------------------
 
-// Creating the canvas (not working)
+let mask = document.createElement('img')
+mask.src = '/images/Mask.png'
 
-let canvas = document.querySelector('canvas')
-console.log(canvas)
-// let ctx = canvas.getContext('2d')
-// canvas.style.border = '2px solid black'
-// let intervalID = 0
+let virus = document.createElement('img')
+virus.src = '/images/Covid.png'
 
-// let mask = document.createElement('img')
-// mask = '/images/Mask.png'
+let lockdown = document.createElement('img')
+lockdown.src ='/images/lockdown.jpeg'
 
-// let virus = document.createElement('img')
-// virus = '/images/Covid.png'
+let party = document.createElement('img')
+party.src = '/images/party.jpeg'
 
-// let lockdown = document.createElement('img')
-// lockdown ='/images/lockdown.jpeg'
+function draw(){
 
-// let party = document.createElement('img')
-// party = '/images/party.jpeg'
+        // always clear canvas
+        ctx.clearRect(0, 0, 1000, 500) 
+        maskX -= 10
+        lockdownX -= 10
+        partyX -= 10
 
-// let bgimg = document.createElement('img')
-// bgimg = '/images/citybackground.jpeg'
+        //draw elements
+        ctx.drawImage(virus, virusX, virusY, 40, 40)
+        ctx.drawImage(mask, maskX, maskY, 80, 40)
+        ctx.drawImage(lockdown, lockdownX, lockdownY, 100, 40)
+        ctx.drawImage(party, partyX, partyY, 100, 40)
+        
+        // update elements
+        
+}
+
+
+
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode == 38 || event.key == "ArrowUp") {
+     isUpArrow = true;
+     isDownArrow = false;
+     virusY -= 10
+  }
+  else if (event.keyCode == 40 || event.key == "ArrowDown") {
+     isUpArrow = false;
+     isDownArrow = true;
+     virusY += 10
+  }
+})
+
+document.addEventListener('keyup', (event) => {
+ isRightArrow = false;
+ isLeftArrow = false;
+})
+
+
+
+
+// function collision(){
+    
+//   // with Mask
+//   if (maskX > canvas.length) {
+//       // change this to a -ve num
+//       incrementX = -incrementX
+//   }  
+  
+//   //check with Lockdown
+//   if (circleY + circleRadius > canvas.height) {
+//       // check if ball stays within paddle boundries
+//       if (circleX > paddleX && circleX < paddleX + paddleWidth) {
+//           incrementY = -incrementY
+//           score++
+//       }
+//       else {
+//           clearInterval(intervalID)
+//           gameOver()
+//       }
+      
+//   }
+//   // with Party
+//   if (circleX - circleRadius < 0) {
+//       // keep this to a +ve num
+//       incrementX = 5
+//   }
+
+//   //check bottom & top 
+//   if (circleY - circleRadius < 0) {
+//       incrementY = 5
+//   }
+// }
+
+
+
+
+
+
+
+
 
 // function draw(){
-//         ctx.drawImage(bgimg, 0, 0)
-//         ctx.drawImage(virus, 50, 50)
-//         ctx.drawImage(mask, 500, 50)
-//         ctx.drawImage(lockdown, 500, 150)
-//         ctx.drawImage(party, 500, 250)
+//   ctx.drawImage(backImg, 0, 0,)
+
+//   for (let i = 0; i < pipes.length; i++){
+//       ctx.drawImage(northPipe, pipes[i].x, pipes[i].y)
+//       ctx.drawImage(southPipe, pipes[i].x, pipes[i].y + constant)
+//       pipes[i].x--
+
+//       if (pipes[i].x == 50) {
+//           pipes.push({
+//               x: canvas.width - 60,
+//               y: - Math.floor(Math.random() * northPipe.height)
+//           })
+//       } 
+
+      
+//       // pipe collision
+//       // if (birdY !== pipes[i].x && pipes[i].y) {
+//       //     clearInterval(intervalID)
+//       //     alert('game over')
+//       // } 
+      
+//   }
+
+//   ctx.drawImage(birdImg, 50, birdY)
+//   ctx.drawImage(fgImg, 0, canvas.height - fgImg.height)
+
+//   ctx.font = '20px Verdana'
+//   ctx.fillText('Score:' + score, 20, canvas.height - 50)
+
+//   birdY += birdIncrement
 // }
+
+// document.addEventListener('mousedown', () => {
+//   birdIncrement = -5
+// })
+
+// document.addEventListener('mouseup', () => {
+//   birdIncrement = 2
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //---------------------------------------------------------------
 //---------------------------------------------------------------
@@ -123,6 +258,19 @@ function deleteSectionTwo () {
 //   }
 // }
 
+// Random placement and x-movement of elements Mask, Lockdown, Party
+// function movementElements (){
+//
+// }
+
+// Moving Viurs along y
+// function moveVirus(){
+//
+// }
+
+// Updating the score
+
+
 
 
 
@@ -131,3 +279,12 @@ function deleteSectionTwo () {
 //---------------------------------------------------------------
 //---------------------------------------------------------------
 
+// END SCREEN
+// function resetButton() {
+// - deletes section
+// - attaches html index
+// }
+
+// function updateScore () {
+// - grabs last updated function score 
+// }
